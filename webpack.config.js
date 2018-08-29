19bc1350b5c3f0bf159643b4b1e81e6997342d6c
nguyenhienluong <42069046+nguyenhienluong@users.noreply.github.com>
2018-08-29 14:38:19 +0700

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 module.exports = {
     entry: './app/controllers/index.ts',
     devtool: 'source-map',
+    mode: 'production',
     module: {
         rules: [
             //Typescript loader
